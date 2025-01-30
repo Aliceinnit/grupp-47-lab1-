1076146be2b1c692f2c9576ecac8405ff8881895
ellalinlokken <118273700+ednamai@users.noreply.github.com>
2025-01-30 20:59:51 +0100

    Update Volvo240Test.java

--- test/Volvo240Test.java
+++ test/Volvo240Test.java
@@ -1,23 +1,50 @@
 package grupp47_lab1.test;
 import grupp47_lab1.*;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.BeforeEach;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class Volvo240Test {
+    Volvo240 volvo;
+
     @BeforeEach
-    void volvo() {
-        Car volvo = new Volvo240();
+    void setUp() {
+        volvo = new Volvo240();
+        volvo.startEngine();
     }
 
     @Test
-    void incrementSpeed() {
-
+    void testIncrementSpeed() {
+        double currentSpeed = volvo.getCurrentSpeed();
+        volvo.incrementSpeed(1);
+        double newSpeed = volvo.getCurrentSpeed();
+        assertTrue(newSpeed >= currentSpeed);
+        //assertTrue(newSpeed <= volvo.getEnginePower());
     }
 
     @Test
-    void decrementSpeed() {
+    void testDecrementSpeed() {
+        double currentSpeed = volvo.getCurrentSpeed();
+        volvo.decrementSpeed(1);
+        double newSpeed = volvo.getCurrentSpeed();
+        assertTrue(newSpeed <= currentSpeed);
+        //assertTrue(newSpeed >= 0);
+    }
+
+    @Test
+    void testGas() {
+        double currentSpeed = volvo.getCurrentSpeed();
+        volvo.gas(2); //Amount parameter set to 2
+        double newSpeed = volvo.getCurrentSpeed();
+        assertEquals(newSpeed, currentSpeed); //Should be equal since amount: 2 is invalid
+    }
+
+    @Test
+    void testBrake() {
+        double currentSpeed = volvo.getCurrentSpeed();
+        volvo.brake(2); //Amount parameter set to 2
+        double newSpeed = volvo.getCurrentSpeed();
+        assertEquals(newSpeed, currentSpeed); //Should be equal since amount: 2 is invalid
     }
 }
