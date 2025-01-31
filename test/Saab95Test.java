a430eb91f5206057f3cc8aef4171fb2f80cb40e8
Aliceinnit <davarialice@gmail.com>
2025-01-31 15:25:59 +0100

    Update Saab95Test.java

--- test/Saab95Test.java
+++ test/Saab95Test.java
@@ -7,34 +7,39 @@ import org.junit.jupiter.api.BeforeEach;
 import static org.junit.jupiter.api.Assertions.*;
 
 class Saab95Test {
-    Saab95Test() {
+    private Saab95 saab;
+    @BeforeEach
+    void setup(){
+        saab = new Saab95(); //Creats new instance before each test.
     }
+
 
     @Test
     void setTurboOn() {
+        saab.setTurboOn();
+        assertTrue(saab.IsTurboOn(), "Turbo should be on.")
     }
 
     @Test
     void setTurboOff() {
+        saab.setTurboOn();
+        saab.setTurboOff();
+        assertFalse(saab.isTurboOn(),"Turbo should be off.")
     }
 
     @Test
     void incrementSpeed() {
+        double startSpeed = saab.getCurrentSpeed();
+        saab.incrementSpeed(1);
+        assertTrue(saab.getCurrentSpeed() > startSpeed, "Speed should increase.")
     }
 
     @Test
     void decrementSpeed() {
+        saab.incrementSpeed(1);
+        double startSpeed = saab.getCurrentSpeed();
+        saab.decrementSpeed(1);
+        assertTrue(saab.getCurrentSpeed() < startSpeed, "Speed should decrease.")
     }
 
-    @Test
-    void move() {
-    }
-
-    @Test
-    void turnLeft() {
-    }
-
-    @Test
-    void turnRight() {
-    }
 }
