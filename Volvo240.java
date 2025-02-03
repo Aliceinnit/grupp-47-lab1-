361e8ed041c142d9632492560d15901868e8b40a
Aliceinnit <davarialice@gmail.com>
2025-02-03 12:58:32 +0100

    Update Volvo240.java

--- Volvo240.java
+++ Volvo240.java
@@ -7,11 +7,7 @@ public class Volvo240 extends Car {
     private final static double trimFactor = 1.25;
 
     public Volvo240(){
-        super();
-        setNrDoors(4);
-        setColor(Color.black);
-        setEnginePower(100);
-        setModelName("Volvo240");
+        super(4,100,Color.black,100,Volvo240);
         stopEngine();
     }
 
